ee20ca7b185c85630a986c432489a91dec8aad5a
Akinbami <tfkakinbami80@yahoo.com>
2023-08-14 04:46:52 -0700

    Update on pycodestyle

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-#0-print_list_integer.py
+# 0-print_list_integer.py
 
 
 def print_list_integer(my_list=[]):
-	""" A function that prints all the integer
-	in a list.
-	No modules should be imported
-	Integers should not be casted into strings
-	"""
-	for i in my_list:
-		print("{:d}".format(i))
+    """ A function that prints all the integer
+    in a list.
+    No modules should be imported
+    Integers should not be casted into strings
+    """
+    for i in my_list:
+        print("{:d}".format(i))
