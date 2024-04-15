aa118baa5cfabdcd1df85fa7bc735454d1c4517c
Akinbami Taofeek <tfkakinbami80@yahoo.com>
2024-04-15 23:51:50 +0100

    Fixed code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,4 @@ def inherits_from(obj, a_class):
        that inherited (directly or indirectly) from the specified class;
        otherwise False.
     """
-    if type(obj) != a_class:
-        return issubclass(type(obj), a_class)
-    else:
-        return False
+    return issubclass(type(obj), a_class)
